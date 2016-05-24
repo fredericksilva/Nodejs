bf62c40b206ed0dedc961bf0b7f1138787040d98
olexy <h.lekan@gmail.com>
2016-05-24 14:33:31 +0100

    [nodeauth] - user registration partb

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -87,5 +90,49 @@ router.post('/register', function(req, res, next) {
             res.redirect('/');
     }
 });
+
+/*passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done){
+        User.getUserByUsername(username, function(err, user){
+            if(err) throw err;
+            if(!user){
+                console.log('Unknown User');
+                return done(null, false, {message: 'Unknown User'});
+            }
+
+            User.comparePassword(password, user.password, function(err, isMatch){
+                if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                } else {
+                    console.log('Invalid Password');
+                    return done(null, false, {message: 'Invalid Password'});
+                }
+            });
+        });
+    }
+));
+
+router.post('/login', passport.authenticate('local',{failureRedirect: '/users/login', failureFlash: 'Invalid username or password'}), function(req, res){
+    console.log('Authentication Successful');
+    req.flash('success', 'You are logged in');
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success', 'You have logged out');
+    res.redirect('/users/login');
+});*/
 
 module.exports = router;
