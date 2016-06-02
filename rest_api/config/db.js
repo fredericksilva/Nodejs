27f499660cf53f13b8ab0273fd968776f53288e2
olexy <h.lekan@gmail.com>
2016-06-02 14:02:12 +0100

    [REST-API] - connecting to mongodb

--- rest_api/config/db.js
+++ rest_api/config/db.js
@@ -1,1 +1,5 @@
 var db = require('mongoose');
+
+db.connect('mongodb://test:pass@ds021663.mlab.com:21663/workouts')
+
+module.exports = db;
---
+++ rest_api/routes/users.js
@@ -0,0 +1,2 @@
+var router = require('express').Router();
+var bcrypt = require('bcryptjs');
