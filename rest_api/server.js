19674a067cb47a27cba6c879fd8fff2d9dc531ce
olexy <h.lekan@gmail.com>
2016-06-02 13:46:03 +0100

    [REST-API] - test route

---
+++ rest_api/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use('/test', function(req, res){
+	res.send('hello world');
+});
+
+app.listen(3000, function(){
+	console.log('app is listening on port 3000...');
+});
