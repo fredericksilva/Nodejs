e68e520fd6ce75e0fdf2f202722f0a4a7857cc41
olexy <h.lekan@gmail.com>
2016-06-03 11:22:46 +0100

    adding a login route and sessions

--- rest_api/server.js
+++ rest_api/server.js
@@ -13,6 +13,7 @@ app.use('/test', function(req, res){
 });
 
 app.use('/api/users', require('./routes/users'));
+app.use('/api/login', require('./routes/sessions'));
 
 app.listen(3000, function(){
 	console.log('app is listening on port 3000...');
