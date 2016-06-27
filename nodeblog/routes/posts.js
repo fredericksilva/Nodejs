623500735b90cf325725bcf28e4c04c7f3ae3229
olexy <h.lekan@gmail.com>
2016-06-27 13:15:59 +0100

    single post comment

--- nodeblog/routes/posts.js
+++ nodeblog/routes/posts.js
@@ -3,6 +3,18 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+
+router.get('/show/:id', function(req, res, next) {
+
+    var posts = db.get('posts');
+
+    posts.findById(req.params.id, function(err, post){
+        res.render('show',{
+            "post": post
+        });
+    });
+});
+
 router.get('/add', function(req, res, next) {
     var categories = db.get('categories');
 
@@ -77,6 +89,64 @@ router.post('/add', function(req, res, next){
     req.checkBody('username','Username field is required').notEmpty();
     req.checkBody('password','Password field is required').notEmpty();
     req.checkBody('password2','Password do not match').equals(req.body.password);
+
+});
+
+router.post('/addcomment', function(req, res, next){
+    // Ger form values
+    var name          = req.body.name;
+    var email         = req.body.email;
+    var body          = req.body.body;
+    var postid        = req.body.postid;
+    var commentdate   = new Date();
+
+    // Form Validation
+
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email is not formatted correctly').isEmail();
+    req.checkBody('body','Body field is required').notEmpty();
+
+    // Check Errors
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        var posts = db.get('posts');
+        potsts.findById(postid, function(err, post){
+            res.render('show', {
+                "erors": errors,
+                "post": post
+            });
+        });
+
+    } else {
+        var comment = {
+            "name": name,
+            "email": email,
+            "body": body,
+            "commentdate": commentdate
+        };
+
+        var posts = db.get('posts');
+
+        posts.update({
+            "_id": postid
+        },{
+            $push:{
+                "comments": comment
+            }
+        }, function(err, doc){
+            if(err){
+                throw err;
+            } else {
+                req.flash('success', 'Comment Added');
+                res.location('/posts/show/'+postid);
+                res.redirect('/posts/show/'+postid);
+            }
+        });
+
+    }
 
 });
 
