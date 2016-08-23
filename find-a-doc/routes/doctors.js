0cb0d5ae753ab724c5e1939bd9a18ce037f432d0
olexy <h.lekan@gmail.com>
2016-08-23 10:09:36 +0100

    reading from cassandra database

--- find-a-doc/routes/doctors.js
+++ find-a-doc/routes/doctors.js
@@ -1,8 +1,43 @@
+
 var express = require('express');
 var router = express.Router();
+var cassandra = require('cassandra-driver');
+var util = require('util');
+
+var client = new cassandra.Client({
+    contactPoints:['127.0.0.1']
+});
+
+client.connect(function(err, result){
+    console.log('Cassandra Connected');
+});
+
 
 router.get('/', function(req, res, next) {
-  res.render('doctors');
+    if(req.query.state){
+        var query = "SELECT * FROM findadoc.doctors WHERE state = ?";
+        client.execute(query, [], function(err, results){
+            if (err){
+                res.status(404).send({msg:err});
+            } else {
+                res.render('doctors', {
+                    doctors: results.rows
+                });
+            }
+        });
+    } else {
+        var query = "SELECT * FROM findadoc.doctors";
+        client.execute(query, [], function(err, results){
+            if (err){
+                res.status(404).send({msg : err});
+            } else {
+                res.render('doctors', {
+                    doctors: results.rows
+                });
+            }
+        });
+    }
+
 });
 
 router.get('/add', function(req, res, next) {
@@ -10,8 +45,29 @@ router.get('/add', function(req, res, next) {
 });
 
 router.get('/details/:id', function(req, res, next) {
-  res.render('details');
+    var query = "SELECT * FROM findadoc.doctors WHERE doc_id = ?";
+    client.execute(query, [req.params.id], function(err, result){
+        if (err){
+            res.status(404).send({msg:err});
+        } else {
+            res.render('details', {
+                doctor: result.rows['0']
+            });
+        }
+    });
 });
 
+router.get('/category/:name', function(req, res, next) {
+    var query = "SELECT * FROM findadoc.doctors WHERE category = ?";
+    client.execute(query, [req.params.name], function(err, results){
+        if (err){
+            res.status(404).send({msg:err});
+        } else {
+            res.render('doctors', {
+                doctors: results.rows
+            });
+        }
+    });
+});
 
 module.exports = router;
