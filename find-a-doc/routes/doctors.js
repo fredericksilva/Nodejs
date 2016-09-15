5665ccfcb8cf98749c4b345acda1c15e78f2ae75
olexy <h.lekan@gmail.com>
2016-09-15 08:25:33 +0100

    re-commit

--- find-a-doc/routes/doctors.js
+++ find-a-doc/routes/doctors.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var cassandra = require('cassandra-driver');
@@ -16,7 +15,7 @@ client.connect(function(err, result){
 router.get('/', function(req, res, next) {
     if(req.query.state){
         var query = "SELECT * FROM findadoc.doctors WHERE state = ?";
-        client.execute(query, [], function(err, results){
+        client.execute(query, [req.query.state], function(err, results){
             if (err){
                 res.status(404).send({msg:err});
             } else {
@@ -41,7 +40,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('adddoctor');
+    var query = "SELECT * FROM findadoc.categories";
+    client.execute(query, [], function(err, results){
+        if (err){
+            res.status(404).send({msg : err});
+        } else {
+
+
+            console.log(util.inspect(results.rows, false, null));
+
+            res.render('adddoctor', {
+                categories: results.rows
+            });
+        }
+    });
+});
+
+router.post('/add', function(req, res, next) {
+  var doc_id = cassandra.types.uuid();
+  var query = "INSERT INTO findadoc.doctors(doc_id, full_name, category, new_patients, graduation_year, practice_name, street_address, city, state, zip) VALUES(?,?,?,?,?,?,?,?,?,?)";
+
+  client.execute(query, [doc_id, req.body.full_name, req.body.category, req.body.new_patients, req.body.graduation_year, req.body.practice_name, req.body.street_address, req.body.city, req.body.state, req.body.zip], {prepare: true},
+  function(err, results){
+      if(err){
+          res.status(404).send({msg:err});
+      } else {
+          req.flash('success', "Doctor Added");
+          res.location('/doctors');
+          res.redirect('/doctors');
+      }
+  });
 });
 
 router.get('/details/:id', function(req, res, next) {
@@ -69,5 +97,6 @@ router.get('/category/:name', function(req, res, next) {
         }
     });
 });
+
 
 module.exports = router;
