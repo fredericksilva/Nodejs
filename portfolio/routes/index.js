d243cf403b2e22ce42c8a4becce6a3427bea835b
olexy <h.lekan@gmail.com>
2016-09-16 10:18:01 +0100

    frontend project display

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'mysql',
+  database: 'portfolio'
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  connection.query('SELECT * FROM projects', function(err, rows, fields){
+    if(err) throw err;
+    res.render('index', {
+      "rows": rows
+    });
+  });
 });
 
 module.exports = router;
