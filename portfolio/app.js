5be957ca4dd95a81ba6f8c3ee4f1387a92263fda
olexy <h.lekan@gmail.com>
2016-09-23 09:37:20 +0100

    create backend to add project

--- portfolio/app.js
+++ portfolio/app.js
@@ -8,20 +8,32 @@ var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var multer = require('multer');
 var session = require('express-session');
-
+var crypto = require('crypto');
+var mime = require('mime');
+  
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var admin = require('./routes/admin');
 
 var app = express();
-
+var storage = multer.diskStorage({
+  destination: './public/img/portfolio/',
+  filename: function (req, file, cb) {
+    crypto.randomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + "." + mime.extension(file.mimetype));
+    });
+  }
+});
 app.use(multer({
-    dest: './public/img/portfolio/'
-}).single(''));
+    storage: storage
+}).single('projectimage'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', {
+    layout: 'partials/layout'
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -38,8 +50,6 @@ app.use(session({
     resave: true
 }));
 
-app.use(flash());
-
 app.locals.moment = require('moment');
 app.use(function(req, res, next){
     res.locals.messages = require('express-messages')(req, res);
@@ -55,7 +65,7 @@ app.use(expressValidator({
 }));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
