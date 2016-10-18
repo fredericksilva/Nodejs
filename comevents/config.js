8e86c86611b2947a7a348826df7d769092d6ae0c
olexy <h.lekan@gmail.com>
2016-10-18 19:03:25 +0100

    fixed comevents.

---
+++ comevents/config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://lekan:pass@ds029328.mlab.com:29328/comvents'
+};
+exports.companyName = 'Community Events';
+exports.projectName = 'Community Events';
+exports.systemEmail = 'info@lexy.com';
+exports.cryptoKey = 'k3yb0ardc4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Community Events',
+    address: process.env.SMTP_FROM_ADDRESS || 'h.lekan@gmail.com'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || 'h.lekan@gmail.com',
+    password: process.env.SMTP_PASSWORD || 'ajoke94%',
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
