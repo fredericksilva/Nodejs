a374edd47b3006afa89ccf79df2b2df080f27edc
olexy <h.lekan@gmail.com>
2016-10-19 23:00:30 +0100

    using get, post and adding ALL routes to route file

--- bookAPI/app.js
+++ bookAPI/app.js
@@ -1,38 +1,29 @@
-var express = require('express');
-// var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
+var express = require('express'),
+	mongoose = require('mongoose');
+	bodyParser = require('body-parser');
 
 
-// var db;
+var db = mongoose.connect('mongodb://localhost/bookAPI');
 
-// if (process.env.ENV == 'Test'){
-//     db = mongoose.connect('mongodb://localhost/bookAPI_test');
-
-// } else {
-//     db = mongoose.connect('mongodb://localhost/bookAPI');
-// }
-
-// var Book = require('./models/bookModel');
-
+var Book = require('./models/bookModel');
 
 var app = express();
+
 var port = process.env.PORT || 3000;
 
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-// bookRouter = require('./Routes/bookRoutes')(Book);
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-// app.use('/api/books', bookRouter);
-// // app.use('/api/authors', authorRouter);
 
-app.get('/', function(req,res){
-    console.log('Welcome to my App');
-    res.send('Welcome to my App');
+app.use('/api/books', bookRouter); 
+
+
+app.get('/', function(req, res){
+    res.send('welcome to my API!');
 });
 
 app.listen(port, function(){
-    console.log('Gulp is running my app on Port: ' + port);
+    console.log('Gulp is running my app on  PORT: ' + port);
 });
-
-// module.exports = app;
