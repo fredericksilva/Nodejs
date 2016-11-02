cd75dfcc175403c7dd03a77460defc0612c26a00
olexy <h.lekan@gmail.com>
2016-11-02 12:21:28 +0100

    Joining a new room with socket.js

--- chatapp/app.js
+++ chatapp/app.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 
 require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose);
 
-require('./routes/routes.js')(express, app, passport, config);
+require('./routes/routes.js')(express, app, passport, config, rooms);
 
 // app.listen(3000, function(){
 // 	console.log('ChatApp Working on Port 3000');
