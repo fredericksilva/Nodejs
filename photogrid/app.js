9d8abfe6ff6ed1c797d7895ae55ecdbcaca2bdda
olexy <h.lekan@gmail.com>
2016-11-14 13:44:46 +0100

    setting up the upload button handler and ajax

--- photogrid/app.js
+++ photogrid/app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 	path = require('path'),
-	config = require('./config/config.js')
-	// knox = require('knox'),
+	config = require('./config/config.js'),
+	knox = require('knox')
 	// fs = require('fs'),
 	// os = require('os'),
 	// formidable = require('formidable'),
@@ -19,11 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 app.set('host', config.host);
 
-// var knoxClient = knox.createClient({
-// 	key: config.S3AccessKey,
-// 	secret: config.S3Secret,
-// 	bucket: config.S3Bucket
-// })
+var knoxClient = knox.createClient({
+	key: config.S3AccessKey,
+	secret: config.S3Secret,
+	bucket: config.S3Bucket
+})
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
