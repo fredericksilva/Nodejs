ed293d156f9378279537a9dda1600948753463c7
olexy <h.lekan@gmail.com>
2016-11-23 11:06:51 +0100

    create first route

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,6 +1,11 @@
 var express = require('express'); //requiring the express library which must be installed
 
-var app = express(); //assigning app variable to the express module for reference
+var app = express(); //assigning app variable to the express module for reference to the method
+
+app.get('/', function(req, res) {
+	var name = "L3xy";
+	res.json("My name is " + name);
+});
 
 app.listen(3000, function(err) {	// assigning port to the express library and  creating error validation optionally
 	if (err) throw err;
