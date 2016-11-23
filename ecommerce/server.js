adf4c3859a3a3139ed51b37522eb1c0750c71d78
olexy <h.lekan@gmail.com>
2016-11-23 11:18:04 +0100

    loggin user requests with morgan module

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,10 +1,18 @@
 var express = require('express'); //requiring the express library which must be installed
+var morgan = require('morgan'); //logging user requests
 
 var app = express(); //assigning app variable to the express module for reference to the method
+
+//Middleware to invoke morgan
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
 	var name = "L3xy";
 	res.json("My name is " + name);
+});
+
+app.get('/catname', function(req, res) {
+	res.json('batman');
 });
 
 app.listen(3000, function(err) {	// assigning port to the express library and  creating error validation optionally
