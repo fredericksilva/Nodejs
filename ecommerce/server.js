9fd2bb9e7ce72fd848920b872a201b347e0e6dcc
olexy <h.lekan@gmail.com>
2016-11-24 09:42:05 +0100

    configure and connect to mongolab mongodb

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,7 +1,17 @@
 var express = require('express'); //requiring the express library which must be installed
 var morgan = require('morgan'); //logging user requests
+var mongoose = require('mongoose');
 
 var app = express(); //assigning app variable to the express module for reference to the method
+
+//connect to mongolab db
+mongoose.connect('mongodb://root:arinola@ds163387.mlab.com:63387/ecommerce', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
 
 //Middleware to invoke morgan
 app.use(morgan('dev'));
