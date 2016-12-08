b69be1b533def574d0d006ed23eb4db07de6f95f
olexy <h.lekan@gmail.com>
2016-12-08 12:16:29 +0100

    custom js for + & - buttons on product page

--- ecommerce/public/js/custom.js
+++ ecommerce/public/js/custom.js
@@ -40,6 +40,40 @@ $(function() {  //indicate jQuery
     });
   });
 
+  //for + button
+  $(document).on('click', '#plus', function(e) {  //#plus is the id of the form control
+    e.preventDefault(); //prevent page refresh
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+
+    priceValue += parseFloat($('#priceHidden').val());
+    quantity += 1;
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  });
+
+  //for - button
+  $(document).on('click', '#minus', function(e) {
+    e.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+
+    //prevent 0 and negative quantity
+    if (quantity == 1) {
+      priceValue = $('#priceHidden').val();
+      quantity = 1;
+    } else {    // apply subtraction
+      priceValue -= parseFloat($('#priceHidden').val());
+      quantity -= 1;
+    }
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  });
+
 
 
 
