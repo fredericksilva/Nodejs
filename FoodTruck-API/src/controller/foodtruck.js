8eb5553b4d41ee7215eb53f62fba8341eaec99dd
olexy <h.lekan@gmail.com>
2017-02-22 19:21:08 +0100

    modify foodtruck delete endpoint to include review, create endpoint to search foodtruck by meal

--- FoodTruck-API/src/controller/foodtruck.js
+++ FoodTruck-API/src/controller/foodtruck.js
@@ -17,7 +17,7 @@ export default({ config, db }) => {
     newFoodTruck.foodtype = req.body.foodtype;
     newFoodTruck.avgcost = req.body.avgcost;
     newFoodTruck.geometry.coordinates = req.body.geometry.coordinates;
-    
+
     newFoodTruck.save(err => {
       if (err) {
         res.send(err);
@@ -53,6 +53,10 @@ export default({ config, db }) => {
         res.send(err);
       }
       foodtruck.name = req.body.name;
+      foodtruck.foodtype = req.body.foodtype;
+      foodtruck.avgcost = req.body.avgcost;
+      foodtruck.geometry.coordinates = req.body.geometry.coordinates;
+
       foodtruck.save(err => {
         if (err) {
           res.send(err)
@@ -70,7 +74,14 @@ export default({ config, db }) => {
       if (err) {
         res.send(err);
       }
-      res.json({ message: "FoodTruck successfully Removed"})
+      Review.remove({
+        foodtruck: req.params.id
+      }, (err, review) => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: "FoodTruck and Associated Reviews successfully Removed"})
+      });
     });
   });
 
@@ -98,6 +109,28 @@ export default({ config, db }) => {
           res.json({ message: 'Food truck review saved!'});
         });
       });
+    });
+  });
+
+  //get reviews for a specific food truck id
+  // 'v1/foodtruck/reviews/:id'
+  api.get('/reviews/:id', (req, res) => {
+    Review.find({foodtruck: req.params.id}, (err, reviews) => {
+      if (err){
+        res.send(err);
+      }
+      res.json(reviews)
+    });
+  });
+
+  // get food truck for specific foodtypes
+  // '/v1/foodtruck/foodtype/:foodtype'
+  api.get('/foodtype/:foodtype', (req, res) => {
+    FoodTruck.find({foodtype: req.params.foodtype}, (err, foodtruck) => {
+      if (err){
+        res.send(err);
+      }
+      res.json(foodtruck)
     });
   });
 
